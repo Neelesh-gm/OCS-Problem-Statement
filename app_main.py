8b6184f1646eec0cf3d15bde22c4584bf32e00c5
Neelesh-gm <neeleshgautam24553@gmail.com>
2025-02-10 17:05:27 +0530

    second uri from supabase

--- app_main.py
+++ app_main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db=SQLAlchemy()
 def create_app():
     app=Flask(__name__, template_folder="templates")
-    app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:2024EE10710OCS@db.naqsaosszkjxbywwhcao.supabase.co:5432/postgres'
+    app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres.naqsaosszkjxbywwhcao:2024EE10710OCS@aws-0-us-west-1.pooler.supabase.com:6543/postgres'
     db.init_app(app)
     from routes import all_routes
     all_routes(app,db)
